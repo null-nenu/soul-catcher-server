817288afc64fcfa6183526a4af762e7b35bb8de8
Wenqiang Xie <wq.xie@outlook.com>
2020-10-24 14:33:43 +0800

    add music and image for setting

--- src/api/setting/models.py
+++ src/api/setting/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BackgroundMusic(models.Model):
+    file = models.FileField(null=True)
+
+
+class BackgroundImage(models.Model):
+    file = models.ImageField(null=True)
