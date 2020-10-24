0c988d9adac768c94fe525bed5e5484986d142a0
haimian <liuqing9801@gmail.com>
2020-10-24 16:21:51 +0800

    add function:deatils

--- src/api/scale/views.py
+++ src/api/scale/views.py
@@ -1,5 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Evaluation
 from .models import EvaluationRate
 from .models import Question
@@ -19,6 +23,23 @@ from .serializers import EvaluationDetailSerializer
 class EvaluationViewSet(viewsets.ModelViewSet):
     queryset = Evaluation.objects.all()
     serializer_class = EvaluationSerializer
+
+    @action(methods=['get'], detail=True)
+    def details(self, request, pk=None):
+        if pk == None:
+            return Response(status=404)
+        evaQuerySet = Evaluation.objects.get(id=pk)
+        evaluation = EvaluationSerializer(evaQuerySet, many=False)
+        quesQuerySet = Question.objects.filter(evaluation_id=pk)
+        question = QuestionSerializer(quesQuerySet, many=True)
+        questionData = question.data
+        for temp in questionData:
+            optionQuerySet = Option.objects.filter(question_id=temp['id'])
+            option = OptionSerializer(optionQuerySet, many=True)
+            temp['options'] = option.data
+        res = dict(evaluation.data)
+        res['questions'] = questionData
+        return Response(res)
 
 
 class EvaluationRateViewSet(viewsets.ModelViewSet):
