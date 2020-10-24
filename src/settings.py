999b2d42ce5775a7156798170200159a185d1820
Wenqiang Xie <wq.xie@outlook.com>
2020-10-24 11:22:09 +0800

    fix

--- src/settings.py
+++ src/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 'django.db.backends.sqlite3',
         'NAME': 'soul',  # BASE_DIR / 'db.sqlite3',
-        'HOST': 'localhost',
+        'HOST': 'mariadb',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': '1214',
