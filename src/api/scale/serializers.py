3520b85f62981f5cace5b913b5c10a1262a3d3c7
haimian <liuqing9801@gmail.com>
2020-10-24 10:29:53 +0800

    add serializers

--- src/api/scale/serializers.py
+++ src/api/scale/serializers.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 from rest_framework import fields
-from .models import Evaluation
+from .models import *
 
 
 class EvaluationSerializer(serializers.Serializer):
     class Meta:
         model = Evaluation
+        fields = "__all__"
+
+class EvaluationRateSerializer(serializers.Serializer):
+    class Meta:
+        model = EvaluationRate
+        fields = "__all__"
+
+class QuestionSerializer(serializers.Serializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+class OptionSerializer(serializers.Serializer):
+    class Meta:
+        model = Option
+        fields = "__all__"
+
+class EvaluationRecordSerializer(serializers.Serializer):
+    class Meta:
+        model = EvaluationRecord
+        fields = "__all__"
+
+class EvaluationDetailSerializer(serializers.Serializer):
+    class Meta:
+        model = EvaluationDetail
         fields = "__all__"
