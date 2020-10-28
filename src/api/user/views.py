773b8c26f2928d700b3cf48a4ac9c600db29d38e
ayu <1364189811@qq.com>
2020-10-28 19:56:02 +0800

    更新user-destory接口

--- src/api/user/views.py
+++ src/api/user/views.py
@@ -4,12 +4,16 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from ..scale.models import EvaluationRecord
+
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
-    queryset =User.objects.all()
-    serializer_class=UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    @action(methods=['post'],detail=False)
-    def destory(self,request):
+    @action(methods=['post'], detail=False)
+    def destory(self, request):
+        user = User.objects.get(username=request.user)
+        EvaluationRecord.objects.filter(user=user).update(user=None)
         return Response(status=200)
