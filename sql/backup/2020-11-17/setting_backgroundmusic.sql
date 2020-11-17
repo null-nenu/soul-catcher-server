daaf291a37f17cdfaaeb8c32bcaecaaa30aff981
ayu <1364189811@qq.com>
2020-11-17 13:41:48 +0800

    移除20201017 sql backup

--- sql/backup/2020-11-17/setting_backgroundmusic.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `setting_backgroundmusic`(`id`, `file`) VALUES (5, 'musics/after.mp3');
-INSERT INTO `setting_backgroundmusic`(`id`, `file`) VALUES (6, 'musics/background_NItxYlb.mp3');
--- sql/backup/2020-11-17/setting_slogan.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `setting_slogan`(`id`, `text`) VALUES (1, '阳光，源于内心的澄澈');
-INSERT INTO `setting_slogan`(`id`, `text`) VALUES (2, '万物皆有裂痕，那是光照进来的地方');
-INSERT INTO `setting_slogan`(`id`, `text`) VALUES (3, '万物皆有裂痕，是光照进来的地方');
-INSERT INTO `setting_slogan`(`id`, `text`) VALUES (4, '路的尽头终归是温柔和月光');
-INSERT INTO `setting_slogan`(`id`, `text`) VALUES (5, '阳光源于内心的澄澈');
-INSERT INTO `setting_slogan`(`id`, `text`) VALUES (6, '世界与我爱着你');
