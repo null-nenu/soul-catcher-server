52e65d8fe9517e575763f28bb4138fa7edbeb178
Wenqiang Xie <wq.xie@outlook.com>
2020-11-18 00:00:54 +0800

    将收集内容导入数据库

---
+++ sql/slogan_11_17_2020.sql
@@ -0,0 +1,53 @@
+-- --------------------------------------------------------
+-- Host:                         www.ayuuu.cn
+-- Server version:               10.5.6-MariaDB-1:10.5.6+maria~focal - mariadb.org binary distribution
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             11.1.0.6116
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table soul.setting_slogan: ~6 rows (approximately)
+/*!40000 ALTER TABLE `setting_slogan` DISABLE KEYS */;
+INSERT INTO `setting_slogan` (`id`, `text`) VALUES
+	(1, '阳光，源于内心的澄澈'),
+	(2, '万物皆有裂痕，那是光照进来的地方'),
+	(3, '万物皆有裂痕，是光照进来的地方'),
+	(4, '路的尽头终归是温柔和月光'),
+	(5, '阳光源于内心的澄澈'),
+	(6, '世界与我爱着你'),
+	(7, '你笑起来真像好天气'),
+	(8, '路的尽头终归是温柔和月光'),
+	(9, '既然上了生活这条贼船就努力做一个快乐的海盗'),
+	(10, '万物皆有裂痕，那是光照进来的地方'),
+	(11, '真实自有万钧之力'),
+	(12, '阳光源于内心的澄澈'),
+	(13, '山水有相逢，似是故乡来'),
+	(14, '雪是大浪漫，你是小人间'),
+	(15, '你一定会越来越好，因为你从来没有停止过学习的脚步'),
+	(16, '你会有不期而遇的温暖'),
+	(17, '希望你像星辰，善良，明亮'),
+	(18, '别忘了你本质也是个爱笑的人'),
+	(19, '没有完美的人生，不完美才是人生'),
+	(20, '无论怎样心有阳光'),
+	(21, '且听风吟，静待花开'),
+	(22, '爱自己，爱每一个人'),
+	(23, '人只有在害怕的时候才会变得勇敢'),
+	(24, '别害怕，宇宙都温柔'),
+	(25, '星星温柔温柔泛滥，人间至善'),
+	(26, '我追逐的是我满怀期待的心'),
+	(27, '今晚夜色真美'),
+	(28, '既然抓不住时间，那就不负时光'),
+	(29, '你特别好，特别温柔'),
+	(30, '曾经的黑暗都会变成耀眼的光');
+/*!40000 ALTER TABLE `setting_slogan` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
